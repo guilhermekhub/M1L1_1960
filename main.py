46372099ccb74e8f535385583adbc68fb8d126f0
guilhermekhub <guilhermefribeirod@gmail.com>
2026-03-17 20:33:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-# M1L1_1960
+meme_dict = {
+                "CRINGE": "coisa vergonhosa",
+                "PPRT": "papo reto/é serio"
+                "TMJ": "tamo junto"
+}
+word = imput ("Digite uma palavra moderna que você não entende (escreva todo a palavra em letras maiúsculas): ")
+
+if word in meme_dict.keys()
+
+else:
+    print("nao temos esta palavra no nosso banco de dados")
+
